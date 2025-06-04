c86ca9ce8c5fc100aebbe3a711a7de1b6cc229f7
Lwis2804 <luis.enrique.enc@gmail.com>
2025-06-04 07:32:05 -0600

    [ADD] codigo ascendente y descendente para las imagenes

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -11,20 +11,29 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var diceImageViewOne: UIImageView!
-    
     @IBOutlet weak var diceImageViewTwo: UIImageView!
+    
+    var leftDiceValue = 1
+    var rightDiceValue = 4
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        diceImageViewOne.image = UIImage(imageLiteralResourceName: "DiceSix")
+        diceImageViewOne.image = UIImage(imageLiteralResourceName: "DiceOne")
         diceImageViewOne.alpha = 0.5
-        diceImageViewTwo.image = UIImage(imageLiteralResourceName: "DiceTwo")
+        diceImageViewTwo.image = UIImage(imageLiteralResourceName: "DiceSix")
     }
 
 
     @IBAction func rollDIceButton(_ sender: Any) {
-        diceImageViewOne.image = [ UIImage(imageLiteralResourceName: "DiceOne"),UIImage(imageLiteralResourceName: "DiceTwo"), UIImage(imageLiteralResourceName: "DiceThree"), UIImage(imageLiteralResourceName: "DiceFour"), UIImage(imageLiteralResourceName: "DiceFive"),UIImage(imageLiteralResourceName: "DiceSix") ] [5]
+        
+        
+        diceImageViewOne.image = [ UIImage(imageLiteralResourceName: "DiceOne"),UIImage(imageLiteralResourceName: "DiceTwo"), UIImage(imageLiteralResourceName: "DiceThree"), UIImage(imageLiteralResourceName: "DiceFour"), UIImage(imageLiteralResourceName: "DiceFive"),UIImage(imageLiteralResourceName: "DiceSix") ] [leftDiceValue]
       
+        diceImageViewTwo.image = [ UIImage(imageLiteralResourceName: "DiceOne"),UIImage(imageLiteralResourceName: "DiceTwo"), UIImage(imageLiteralResourceName: "DiceThree"), UIImage(imageLiteralResourceName: "DiceFour"), UIImage(imageLiteralResourceName: "DiceFive"),UIImage(imageLiteralResourceName: "DiceSix") ] [rightDiceValue]
+        
+        leftDiceValue = leftDiceValue + 1
+        rightDiceValue = rightDiceValue - 1
+        
     }
 }
 
